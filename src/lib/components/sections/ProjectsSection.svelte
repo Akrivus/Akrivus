08cbce7e2ec03193d761be4954177c1fd1c9bafa
Owen Adrian <akrivus@gmail.com>
2024-02-01 00:24:41 +0000

    Code clean-up and organization.

---
+++ src/lib/components/sections/ProjectsSection.svelte
@@ -0,0 +1,103 @@
+<script>
+  import Section from "$lib/components/Section.svelte";
+  import ThirdImageCard from "$lib/components/cards/smart/ThirdImageCard.svelte";
+
+  import src_portfolio from "$lib/images/projects/portfolio.png";
+  import src_livinglegend from "$lib/images/projects/livinglegend.png";
+  import src_comingsoon from "$lib/images/projects/comingsoon.png";
+  import src_coffeecraft from "$lib/images/projects/coffeecraft.png";
+  import src_kagic from "$lib/images/projects/kagic.png";
+</script>
+
+<Section id="projects" title="My Projects" link="Projects">
+  <ThirdImageCard title="My Portfolio" src={src_portfolio} alt="Early thumbnail of my portfolio.">
+    <p slot="body">
+      In 2024, I developed a new portfolio website to showcase my skills and
+      experience using SvelteKit, a new way to create single-page web front-ends.
+    </p>
+    <div slot="link">
+      <i class="fab fa-github" /> 
+      <a href="https://github.com/Akrivus/Akrivus" class="icon-link">View On GitHub</a>
+    </div>
+  </ThirdImageCard>
+  <ThirdImageCard title="Living Legend" src={src_livinglegend} alt="Procedurally generated island with tide and erosion.">
+    <p slot="body">
+      I'm developing a procedurally generated island with simulated noise,
+      ecosystems, and civilizations that GPT can use to string together epics.
+    </p>
+    <div slot="link">
+      <i class="fab fa-github" /> 
+      <a href="https://github.com/Akrivus/LivingLegend" class="icon-link">View On GitHub</a>
+    </div>
+  </ThirdImageCard>
+  <ThirdImageCard title="PhotoGo" src={src_comingsoon} alt="Image coming soon.">
+    <p slot="body">
+      Upload, share, and comment on photos with PhotoGo, a social media
+      platform powered by Ruby on Rails and CarrierWave.
+    </p>
+    <div slot="link">
+      <i class="fab fa-github" /> 
+      <a href="https://github.com/Akrivus/PhotoGo" class="icon-link">View On GitHub</a>
+    </div>
+  </ThirdImageCard>
+  <ThirdImageCard title="ChatWS" src={src_comingsoon} alt="Image coming soon.">
+    <p slot="body">
+      Send messages to friends with ChatWS, a chat application built with
+      Sinatra to demonstrate proficiency in Ruby and with WebSockets.
+    </p>
+    <div slot="link">
+      <i class="fab fa-github" /> 
+      <a href="https://github.com/Akrivus/ChatWS" class="icon-link">View On GitHub</a>
+    </div>
+  </ThirdImageCard>
+  <ThirdImageCard title="MobiMenu" src={src_comingsoon} alt="Image coming soon.">
+    <p slot="body">
+      Ruby powered display server for restaurants and cafes who don't have a
+      TV with picture mode, but do have a spare Raspberry Pi 4 laying around.
+    </p>
+    <div slot="link">
+      <i class="fab fa-github" /> 
+      <a href="https://github.com/Akrivus/MobiMenu" class="icon-link">View On GitHub</a>
+    </div>
+  </ThirdImageCard>
+  <ThirdImageCard title="RESTfulCraft" src={src_comingsoon} alt="Image coming soon.">
+    <p slot="body">
+      An attempt to teach RESTful API design using Minecraft as a model.
+      Blocks are resources, and the world is your API endpoint.
+    </p>
+    <div slot="link">
+      <i class="fab fa-github" /> 
+      <a href="https://github.com/Akrivus/RESTfulCraft" class="icon-link">View On GitHub</a>
+    </div>
+  </ThirdImageCard>
+  <ThirdImageCard title="Block Party" src={src_comingsoon} alt="Image coming soon.">
+    <p slot="body">
+      A spiritual successor to my popular KAGIC mod, Block Party adds <i>tsukumogami</i>,
+      blocks that come to life and enjoy the world around them.
+    </p>
+    <div slot="link">
+      <i class="fab fa-github" /> 
+      <a href="https://github.com/Akrivus/BlockParty" class="icon-link">View On GitHub</a>
+    </div>
+  </ThirdImageCard>
+  <ThirdImageCard title="CoffeeCraft" src={src_coffeecraft} alt="Image coming soon.">
+    <p slot="body">
+      A Minecraft mod that introduces the coffee industry from the ground
+      up. Grow coffee beans, roast them, build your own cafe and sell to villagers.
+    </p>
+    <div slot="link">
+      <i class="fab fa-github" /> 
+      <a href="https://github.com/Akrivus/CoffeeCraft" class="icon-link">View On GitHub</a>
+    </div>
+  </ThirdImageCard>
+  <ThirdImageCard title="KAGIC" src={src_kagic} alt="Image coming soon.">
+    <p slot="body">
+      A popular Minecraft mod that introduces holographic guardians called
+      Gems. Gems are born to serve, and read chat with rule-based NLP.
+    </p>
+    <div slot="link">
+      <i class="fab fa-github" /> 
+      <a href="https://github.com/kagic" class="icon-link">View On GitHub</a>
+    </div>
+  </ThirdImageCard>
+</Section>
---
+++ src/lib/components/sections/SkillsSection.svelte
@@ -0,0 +1,55 @@
+<script>
+  import Section from "$lib/components/Section.svelte";
+  import QuarterCard from "$lib/components/cards/smart/QuarterCard.svelte";
+</script>
+
+<Section id="skills" title="My Skills" link="Skills">
+  <QuarterCard title="Cloud Development">
+    I develop cloud solutions that are reliable, scalable, and secure.
+  </QuarterCard>
+  <QuarterCard title="Full-Stack Development">
+    I build full-stack applications that enable organizations to succeed.
+  </QuarterCard>
+  <QuarterCard title="Web Development">
+    I write HTML, CSS, and JavaScript that is clean and ready to ship.
+  </QuarterCard>
+  <QuarterCard title="Cloud Infrastructure">
+    I deploy and maintain services on Azure, Heroku, and Digital Ocean.
+  </QuarterCard>
+  <QuarterCard title="Cloud Services">
+    Docker, Kubernetes, Nginx, Azure AKS, Azure App Service, AWS ECS
+  </QuarterCard>
+  <QuarterCard title="Continuous Integration">
+    CI/CD, GitHub Actions, Azure Pipelines, CircleCI, Travis CI
+  </QuarterCard>
+  <QuarterCard title="Database Management">
+    PostgreSQL, SQLite, MongoDB, Redis
+  </QuarterCard>
+  <QuarterCard title="Integrating Security">
+    MFA, SSO, RBAC, along with OWASP, GDPR, and PCI standards
+  </QuarterCard>
+  <QuarterCard title="Ruby Expertise">
+    Ruby on Rails, Sinatra, RSpec, Capybara, DelayedJob, Resque
+  </QuarterCard>
+  <QuarterCard title="C#/.NET Expertise">
+    ASP.NET Core, Entity Framework, NUnit, Moq, Unity, WinForms
+  </QuarterCard>
+  <QuarterCard title="JavaScript Expertise">
+    React, Svelte, Vite, Vue, jQuery, Node.js, Express
+  </QuarterCard>
+  <QuarterCard title="Artificial Intelligence">
+    I leverage ChatGPT and OpenAI to maximize my productivity.
+  </QuarterCard>
+  <QuarterCard title="Team Collaboration">
+    I can collaborate with cross-functional teams to achieve goals.
+  </QuarterCard>
+  <QuarterCard title="Technical Writing">
+    I use my writing skills to document my work and share my knowledge.
+  </QuarterCard>
+  <QuarterCard title="Project Management">
+    I can plan, execute, and deliver projects on time and budget.
+  </QuarterCard>
+  <QuarterCard title="Continuous Learning">
+    I adapt to new challenges by learning new skills and technologies.
+  </QuarterCard>
+</Section>
