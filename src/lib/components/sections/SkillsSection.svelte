dd04c6ea01b253d73d75bc5d2976c58e5ac6d9e9
Owen Adrian <akrivus@gmail.com>
2024-02-20 23:04:17 +0000

    Re-arranged skills.

--- src/lib/components/sections/SkillsSection.svelte
+++ src/lib/components/sections/SkillsSection.svelte
@@ -4,14 +4,38 @@
 </script>
 
 <Section id="skills" title="My Skills" link="Skills">
-  <QuarterCard title="Cloud Development">
-    I develop cloud solutions that are reliable, scalable, and secure.
-  </QuarterCard>
-  <QuarterCard title="Full-Stack Development">
-    I build full-stack applications that enable organizations to succeed.
-  </QuarterCard>
   <QuarterCard title="Web Development">
-    I write HTML, CSS, and JavaScript that is clean and ready to ship.
+    I write JavaScript, HTML and CSS that is clean and ready to ship.
+  </QuarterCard>
+  <QuarterCard title="API Development">
+    I build APIs that empower organizational growth and success.
+  </QuarterCard>
+  <QuarterCard title="Artificial Intelligence">
+    I leverage ChatGPT and Copilot to maximize my productivity.
+  </QuarterCard>
+  <QuarterCard title="Continuous Learning">
+    I adapt to new challenges by learning new skills and technologies.
+  </QuarterCard>
+  <QuarterCard title="Ruby">
+    Ruby on Rails, Sinatra, RSpec, Capybara, DelayedJob, Resque
+  </QuarterCard>
+  <QuarterCard title="JavaScript">
+    React, Svelte, Vite, Vue, jQuery, Node.js, Express
+  </QuarterCard>
+  <QuarterCard title="C#">
+    ASP.NET Core, Entity Framework, NUnit, Moq, Unity, WinForms
+  </QuarterCard>
+  <QuarterCard title="Machine Learning">
+    I use ML and AI to solve complex problems and automate tasks.
+  </QuarterCard>
+  <QuarterCard title="Project Management">
+    I can plan, execute, and deliver projects on time and budget.
+  </QuarterCard>
+  <QuarterCard title="Team Collaboration">
+    I can collaborate with cross-functional teams to achieve goals.
+  </QuarterCard>
+  <QuarterCard title="Technical Writing">
+    I use my writing skills to document my work and share my knowledge.
   </QuarterCard>
   <QuarterCard title="Cloud Infrastructure">
     I deploy and maintain services on Azure, Heroku, and Digital Ocean.
@@ -27,29 +51,5 @@
   </QuarterCard>
   <QuarterCard title="Integrating Security">
     MFA, SSO, RBAC, along with OWASP, GDPR, and PCI standards
-  </QuarterCard>
-  <QuarterCard title="Ruby Expertise">
-    Ruby on Rails, Sinatra, RSpec, Capybara, DelayedJob, Resque
-  </QuarterCard>
-  <QuarterCard title="C#/.NET Expertise">
-    ASP.NET Core, Entity Framework, NUnit, Moq, Unity, WinForms
-  </QuarterCard>
-  <QuarterCard title="JavaScript Expertise">
-    React, Svelte, Vite, Vue, jQuery, Node.js, Express
-  </QuarterCard>
-  <QuarterCard title="Artificial Intelligence">
-    I leverage ChatGPT and OpenAI to maximize my productivity.
-  </QuarterCard>
-  <QuarterCard title="Team Collaboration">
-    I can collaborate with cross-functional teams to achieve goals.
-  </QuarterCard>
-  <QuarterCard title="Technical Writing">
-    I use my writing skills to document my work and share my knowledge.
-  </QuarterCard>
-  <QuarterCard title="Project Management">
-    I can plan, execute, and deliver projects on time and budget.
-  </QuarterCard>
-  <QuarterCard title="Continuous Learning">
-    I adapt to new challenges by learning new skills and technologies.
   </QuarterCard>
 </Section>
