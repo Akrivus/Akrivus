d33672eb14d8c438f5f63e0b3cbee44abd5e6d83
Owen Adrian <akrivus@gmail.com>
2024-03-26 14:59:09 +0000

    Updated blurb to sound less like "Meet The Engineer."

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -24,13 +24,14 @@
   <div slot="lead">
     <h1>Hi,<br /><span class="name">I'm Owen</span></h1>
     <p class="lead">
-      I'm a software engineer. For over seven years, I've dedicated my time to
-      enabling success using my technical expertise to produce reliable, scalable,
-      and secure cloud solutions.
+      Since 2017, I've overcome challenges using my passion for learning and drive to create intuitive solutions for the web.
+      As an entrepreneur, I know how important it is to be able to pivot quickly and adapt to new technologies, while maintaining
+      a high standard of quality, which I internalize and maintain as a software engineer.
     </p>
     <a href="Owen-Adrian-Software-Engineer-Resume.pdf" class="btn btn-lg btn-success">Résumé</a>
     <a href="https://linkedin.com/in/owen-adrian" class="btn btn-lg btn-success" aria-label="LinkedIn"><i class="fab fa-linkedin" /></a>
     <a href="https://github.com/Akrivus" class="btn btn-lg btn-success" aria-label="GitHub"><i class="fab fa-github" /></a>
+    <a href="https://www.youtube.com/channel/UCA52QVCQ0NI9QWBWpx-aSOg" class="btn btn-lg btn-success" aria-label="YouTube"><i class="fab fa-youtube" /></a>
     <ScrollButton />
   </div>
   <div class="col" slot="side">
@@ -40,7 +41,7 @@
 
 <NavArea>
   <AboutSection />
+  <ProjectsSection />
   <SkillsSection />
   <JourneySection />
-  <ProjectsSection />
 </NavArea>
