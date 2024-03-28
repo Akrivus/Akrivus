54aecc4af9971118b8b77b6881b15978d0e53b4d
Owen Adrian <akrivus@gmail.com>
2024-03-28 17:42:34 +0000

    Updated blurb.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -24,9 +24,10 @@
   <div slot="lead">
     <h1>Hi,<br /><span class="name">I'm Owen</span></h1>
     <p class="lead">
-      Since 2017, I've overcome challenges using my passion for learning and drive to create intuitive solutions for the web.
-      As an entrepreneur, I know how important it is to be able to pivot quickly and adapt to new technologies, while maintaining
-      a high standard of quality, which I internalize and maintain as a software engineer.
+      I'm driven by a passion for creating and learning. I've been
+      building solutions for 7 years, as an entrepreneur and software engineer.
+      My goal is to build solutions that make technology more accessible and
+      enjoyable for everyone.
     </p>
     <a href="Owen-Adrian-Software-Engineer-Resume.pdf" class="btn btn-lg btn-success">Résumé</a>
     <a href="https://linkedin.com/in/owen-adrian" class="btn btn-lg btn-success" aria-label="LinkedIn"><i class="fab fa-linkedin" /></a>
