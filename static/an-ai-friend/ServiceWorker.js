ae359d15eb21aa185a5025c567f2ff0b82fcb7e0
Owen Adrian <akrivus@gmail.com>
2024-06-10 15:59:40 -0700

    Updated resume + added demo.

---
+++ static/an-ai-friend/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "Syntreno-Demo-b9d25cc0-7308-4cbe-b328-70c72835bb61";
+const contentToCache = [
+    "Build/an-ai-friend.loader.js",
+    "Build/an-ai-friend.framework.js",
+    "Build/an-ai-friend.data",
+    "Build/an-ai-friend.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
